9e7f29fba3ee78879b46657aa511e6eff8c5ff46
Aidan Horn <49376700+HyperionPanda@users.noreply.github.com>
2022-03-14 15:39:38 -0500

    Now fixed view issue

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,16 +7,30 @@ public class FollowPlayer : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     private GameObject emp;
+    private GameObject view;
+    private GameObject inside;
+    private GameObject inside1;
+    private GameObject inside2;
+    private GameObject inside3;
+    private GameObject inside4;
     private GameObject checkLevel;
     private int currentlevel;
+    private GameObject current;
     // Start is called before the first frame update
     void Start()
     {
+        current = this.gameObject;
         checkLevel = GameObject.Find("LevelController");
        
         if (checkLevel.GetComponent<BeginControl>().level == 5)
         {
+            view = GameObject.Find("View Border");
             emp = GameObject.Find("security guard");
+            inside = GameObject.Find("Inside border");
+            inside1 = GameObject.Find("Inside border (1)");
+            inside2 = GameObject.Find("Inside border (2)");
+            inside3 = GameObject.Find("Inside border (3)");
+            inside4 = GameObject.Find("Inside border (4)");
         }
     }
 
@@ -24,7 +38,7 @@ public class FollowPlayer : MonoBehaviour
 
     void Update()
     {
-        if (checkLevel.GetComponent<BeginControl>().level == 5 && this.gameObject == emp)
+        if (checkLevel.GetComponent<BeginControl>().level == 5 && (current == emp || current == view || current == inside || current == inside1 || current == inside2 || current == inside3 || current == inside4))
         {
             if (emp.GetComponent<MoveUrAss>().direction == "left")
             {
@@ -37,7 +51,7 @@ public class FollowPlayer : MonoBehaviour
 
             }
         }
-        else if (this.gameObject == emp)
+        else 
         {
             transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
         }
